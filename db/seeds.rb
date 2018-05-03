be3278a992432641e30eec279bce256e56072787
Jude Lawson <lawsonjdutton@gmail.com>
2018-05-02 22:27:50 -0600

    Updates seeds to include Items seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
 require './db/csv/csv_wizard'
 require './app/models/merchant.rb'
+require './app/models/item.rb'
 merchants = CSVWizard.read_file('./data/merchants.csv')
 merchants.each do |merchant|
   Merchant.create(id:         merchant[:id],
                   name:       merchant[:name],
                   created_at: merchant[:created_at],
                   updated_at: merchant[:updated_at])
+end
+
+items = CSVWizard.read_file('./data/items.csv')
+items.each do |item|
+  Item.create(id:          item[:id],
+              title:        item[:name],
+              description: item[:description],
+              price:  item[:unit_price])
 end
