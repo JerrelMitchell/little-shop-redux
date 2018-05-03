95bf49f23b2cab66cc48efe1b098e0466c893768
JerrelMitchell <jerrelmitchell@ymail.com>
2018-05-03 17:08:50 -0600

    add new paths for create, show, edit, and their needed redirects.

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -1,7 +1,45 @@
 class LittleShopApp < Sinatra::Base
-  get ['/', '/merchants'] do
+  get ['/', '/merchants', '/merchants/'] do
     @merchants = Merchant.all
 
-    erb :"merchant/index"
+    erb :"merchants/index"
+  end
+
+  get '/merchants/new' do
+    erb :'merchants/create'
+  end
+
+  get '/merchants/:id' do
+    @merchant = Merchant.find(params['id'])
+
+    erb :"merchants/show"
+  end
+
+  get '/merchants/:id/edit' do
+    @merchant = Merchant.find(params['id'])
+    erb :"merchants/edit"
+  end
+
+  delete '/merchants/:id/delete' do
+    merchant = Merchant.find(params['id'])
+
+    Merchant.delete(merchant)
+
+    redirect '/merchants'
+  end
+
+  post '/merchants' do
+    new_id = Merchant.last.id + 1
+    params['merchant'] = new_id
+    merchant = Merchant.create(params['merchant'])
+
+    redirect "/merchants/#{merchant.id}"
+  end
+
+  post '/merchants/:id' do
+    merchant = Merchant.find(params['id'])
+    merchant.update(params['merchant'])
+    merchant.save
+    redirect "/merchants/#{merchant.id}"
   end
 end
