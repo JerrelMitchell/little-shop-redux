bf7df0bbad27a86f7bf3020c1cc96b3003325116
John Livingston <jclivingston316@gmail.com>
2018-05-05 19:59:07 -0600

    ads invoices.erb file

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -9,4 +9,25 @@ class LittleShopApp < Sinatra::Base
         @invoice = Invoice.find(params['id'])
         erb :"merchants/show"
     end
+
+    get '/invoices/:id/edit' do
+        @invoice = Invoice.find(params['id'])
+        erb :"invoices/edit"
+    end
+
+    delete '/invoices/:id/delete' do
+        invoice = Invoice.find(params['id'])
+
+        Invoice.delete(invoice)
+
+        redirect '/invoices'
+    end
+
+    post '/invoices/:id' do
+        invoice = Invoice.find(params['id'])
+        invoice.update(params['invoice'])
+        invoice.save
+        redirect "/invoices/#{invoice.id}"
+    end
 end
+
