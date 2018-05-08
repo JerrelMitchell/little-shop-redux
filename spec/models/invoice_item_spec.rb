e4d258f98121516cfba1fe83649e1a55f7ea6875
John Livingston <jclivingston316@gmail.com>
2018-05-08 10:09:28 -0600

    adds invoice items relationship tests

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -26,4 +26,28 @@ RSpec.describe InvoiceItem do
       expect(invoice_item).to_not be_valid
     end
   end
+  describe 'invoice items relationships' do
+
+    before(:each) do
+      @invoice1 = Invoice.create(merchant_id: 12334111, status: 'pending')
+      @invoice2 = Invoice.create(merchant_id: 12334112, status: 'shipped')
+      @invoice3 = Invoice.create(merchant_id: 12334113, status: 'returned')
+      @first_item_attrs = { id: 1, title: 'The First Cool Item',
+                         description: 'It\'s alright',
+                         price: 357, image: 'imgs/img1' } 
+      @second_item_attrs = { id: 2, title: 'The Second Cool Item',
+                           description: 'It\'s cool',
+                           price: 1000, image: 'imgs/img2'}
+      @third_item_attrs = { id: 3, title: 'The Third Cool Item',
+                          description: 'It\'s the coolest',
+                          price: 900, image: 'imgs/img3'}
+      Item.create(@first_item_attrs)
+      Item.create(@second_item_attrs)
+      Item.create(@third_item_attrs)
+    end
+    it 'should give invoice acccess to items through item_id' do
+
+
+    end
+  end
 end
