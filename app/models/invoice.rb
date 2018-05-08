f4737d639c4b2e8dccd711490fde74aeda78718e
John Livingston <jclivingston316@gmail.com>
2018-05-08 14:49:17 -0600

    adds relationship between invoice and merchant

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,7 @@
 class Invoice < ActiveRecord::Base
     validates :merchant_id, presence: true
     validates :status, presence: true
-
+    belongs_to :merchant
     has_many(:invoice_items)
     has_many(:items, through: :invoice_items)
 end
